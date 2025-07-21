b98bc73e351eaf31ccf70ee2e76ee5769e9a45b7
defalt <52076521+DEFRO5@users.noreply.github.com>
2025-07-21 15:22:25 +0000

    added evolution overlay

--- device.mk
+++ device.mk
@@ -50,13 +50,15 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
+    
 
 # Sensors
 PRODUCT_PACKAGES += \
